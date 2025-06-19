d782464e8d5f9b5fc63096b33922405daba93902
BigSteveAbdulmex <steveabdulmexa@gmail.com>
2025-06-19 12:27:36 +0100

    Refactored the Investment Calculator app

--- Investment-Calculator/investment_calculator.go
+++ Investment-Calculator/investment_calculator.go
@@ -5,23 +5,33 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
 	var investmentAmount float64
 	var years float64
 	var expectedReturnRate float64
 
-	fmt.Print("Enter investment amount: ")
+	outputText("Enter investment amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Enter expected return rate: ")
+	outputText("Enter expected return rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Enter number of years: ")
+	outputText("Enter number of years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
 	fmt.Printf("Future Value: %.2f\nFuture Value (adjusted for inflation): %.2f\n", futureValue, futureRealValue)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (fv float64, rfv float64) {
+	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv = fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
