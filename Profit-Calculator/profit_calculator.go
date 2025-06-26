a0457dd7ed71b389933ab349d712f8f90a5b71a9
BigSteveAbdulmex <steveabdulmexa@gmail.com>
2025-06-26 14:18:34 +0100

    Added persistency to the Profit Calculator App

--- Profit-Calculator/profit_calculator.go
+++ Profit-Calculator/profit_calculator.go
@@ -1,25 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenue := getUserInput("Enter revenue: ")
-	expenses := getUserInput("Enter expenses: ")
-	taxRate := getUserInput("Enter tax rate: ")
+	revenue, err := getUserInput("Enter revenue: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	expenses, err := getUserInput("Enter expenses: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	taxRate, err := getUserInput("Enter tax rate: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	ebt, eat, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Printf("Earnings before tax: %.1f\n", ebt)
 	fmt.Printf("Earnings after tax: %.1f\n", eat)
 	fmt.Printf("Ratio of earnings before and after tax: %.3f\n", ratio)
+	storeCalculatedResultsToFile(ebt, eat, ratio)
 
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-	return userInput
+
+	if userInput <= 0 {
+		return 0, errors.New("value must be a positive number")
+	}
+
+	return userInput, nil
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
@@ -30,4 +57,9 @@ func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, 
 	// Ratio of ebt and eat
 	ratio := ebt / eat
 	return ebt, eat, ratio
+}
+
+func storeCalculatedResultsToFile(ebt, eat, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nEAT: %.1f\nRatio: %.3f\n", ebt, eat, ratio)
+	os.WriteFile("profits.txt", []byte(results), 0644)
 }
