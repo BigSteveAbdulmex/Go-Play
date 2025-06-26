eca6c521fee8ebf4b76f889189474d176c9274d3
BigSteveAbdulmex <steveabdulmexa@gmail.com>
2025-06-26 12:13:36 +0100

    Hardcoded version of banking app

--- Banking-App/bank.go
+++ Banking-App/bank.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("What'd you like to do today?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit app")
+
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	fmt.Println("Your choise: ", choice)
+}
