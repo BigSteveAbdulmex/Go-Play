f1241606fd97f33846ecdfdfb3557b531adf0092
BigSteveAbdulmex <steveabdulmexa@gmail.com>
2025-06-26 13:00:46 +0100

    Completed demo version 3 of the Banking App

--- Banking-App/bank.go
+++ Banking-App/bank.go
@@ -6,51 +6,57 @@ func main() {
 	var accountBalance = 1000.0
 
 	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("What'd you like to do today?")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit app")
 
-	var choice int
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
+	for {
 
-	if choice == 1 {
-		fmt.Println("Your balance is:", accountBalance)
-	} else if choice == 2 {
-		fmt.Print("Your deposit:")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
+		fmt.Println("What'd you like to do today?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit app")
 
-		if depositAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0")
-			return
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+
+		if choice == 1 {
+			fmt.Println("Your balance is:", accountBalance)
+		} else if choice == 2 {
+			fmt.Print("Your deposit:")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0")
+				continue
+			}
+
+			accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
+			fmt.Println("Balanca updated! New balance:", accountBalance)
+		} else if choice == 3 {
+			fmt.Print("Withdrawal amount:")
+			var withdrawalAmount float64
+			fmt.Scan(&withdrawalAmount)
+
+			if withdrawalAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0")
+				return
+			}
+
+			if withdrawalAmount > accountBalance {
+				fmt.Println("Invalid amount. You can't withdraw more than you have")
+				return
+			}
+
+			accountBalance -= withdrawalAmount
+			fmt.Println("You have withdrawed:", withdrawalAmount)
+			fmt.Println("Your new balance is:", accountBalance)
+		} else {
+			fmt.Println("Have a good day!")
+			break
 		}
 
-		accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
-		fmt.Println("Balanca updated! New balance:", accountBalance)
-	} else if choice == 3 {
-		fmt.Print("Withdrawal amount:")
-		var withdrawalAmount float64
-		fmt.Scan(&withdrawalAmount)
-
-		if withdrawalAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0")
-			return
-		}
-
-		if withdrawalAmount > accountBalance {
-			fmt.Println("Invalid amount. You can't withdraw more than you have")
-			return
-		}
-
-		accountBalance -= withdrawalAmount
-		fmt.Println("You have withdrawed:", withdrawalAmount)
-		fmt.Println("Your new balance is:", accountBalance)
-	} else {
-		fmt.Println("Have a good day!")
 	}
 
-	// fmt.Println("Your choise: ", choice)
+	fmt.Println("Thanks for chosing Go Bank!")
 }
